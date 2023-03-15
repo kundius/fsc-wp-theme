e27040d5103aa784f71e6331cccf13674d1b77d0
kundius <kundius.ruslan@gmail.com>
2023-03-15 15:56:09 +0300

    deploy

--- inc/Services/PostTypes.php
+++ inc/Services/PostTypes.php
@@ -79,50 +79,50 @@ class PostTypes implements Service
             //'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
             'hierarchical'        => false,
             'supports'            => ['title', 'editor', 'thumbnail', 'excerpt'], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
-            'taxonomies'          => ['category'],
+            'taxonomies'          => ['project_category'],
             'has_archive'         => false,
             'rewrite'             => true,
             'query_var'           => true,
         ]);
 
         
-        // register_taxonomy( 'taxonomy', [ 'post' ], [
-        //     'label'                 => '', // определяется параметром $labels->name
-        //     'labels'                => [
-        //         'name'              => 'Genres',
-        //         'singular_name'     => 'Genre',
-        //         'search_items'      => 'Search Genres',
-        //         'all_items'         => 'All Genres',
-        //         'view_item '        => 'View Genre',
-        //         'parent_item'       => 'Parent Genre',
-        //         'parent_item_colon' => 'Parent Genre:',
-        //         'edit_item'         => 'Edit Genre',
-        //         'update_item'       => 'Update Genre',
-        //         'add_new_item'      => 'Add New Genre',
-        //         'new_item_name'     => 'New Genre Name',
-        //         'menu_name'         => 'Genre',
-        //         'back_to_items'     => '← Back to Genre',
-        //     ],
-        //     'description'           => '', // описание таксономии
-        //     'public'                => true,
-        //     // 'publicly_queryable'    => null, // равен аргументу public
-        //     // 'show_in_nav_menus'     => true, // равен аргументу public
-        //     // 'show_ui'               => true, // равен аргументу public
-        //     // 'show_in_menu'          => true, // равен аргументу show_ui
-        //     // 'show_tagcloud'         => true, // равен аргументу show_ui
-        //     // 'show_in_quick_edit'    => null, // равен аргументу show_ui
-        //     'hierarchical'          => false,
+        register_taxonomy('project_category', ['project'], [
+            'label'                 => '', // определяется параметром $labels->name
+            'labels'                => [
+                'name'              => 'Категории',
+                'singular_name'     => 'Категория',
+                'search_items'      => 'Искать категории',
+                'all_items'         => 'Все категории',
+                'view_item '        => 'Смотреть категорию',
+                'parent_item'       => 'Родительская категория',
+                'parent_item_colon' => 'Родительская категория:',
+                'edit_item'         => 'Редактировать категорию',
+                'update_item'       => 'Обновить категорию',
+                'add_new_item'      => 'Новая категория',
+                'new_item_name'     => 'Новое название категории',
+                'menu_name'         => 'Категории',
+                'back_to_items'     => 'Вернуться к категориям',
+            ],
+            'description'           => '', // описание таксономии
+            'public'                => true,
+            // 'publicly_queryable'    => null, // равен аргументу public
+            // 'show_in_nav_menus'     => true, // равен аргументу public
+            // 'show_ui'               => true, // равен аргументу public
+            // 'show_in_menu'          => true, // равен аргументу show_ui
+            // 'show_tagcloud'         => true, // равен аргументу show_ui
+            // 'show_in_quick_edit'    => null, // равен аргументу show_ui
+            'hierarchical'          => false,
 
-        //     'rewrite'               => true,
-        //     //'query_var'             => $taxonomy, // название параметра запроса
-        //     'capabilities'          => array(),
-        //     'meta_box_cb'           => null, // html метабокса. callback: `post_categories_meta_box` или `post_tags_meta_box`. false — метабокс отключен.
-        //     'show_admin_column'     => false, // авто-создание колонки таксы в таблице ассоциированного типа записи. (с версии 3.5)
-        //     'show_in_rest'          => null, // добавить в REST API
-        //     'rest_base'             => null, // $taxonomy
-        //     // '_builtin'              => false,
-        //     //'update_count_callback' => '_update_post_term_count',
-        // ] );
+            'rewrite'               => true,
+            //'query_var'             => $taxonomy, // название параметра запроса
+            'capabilities'          => [],
+            'meta_box_cb'           => null, // html метабокса. callback: `post_categories_meta_box` или `post_tags_meta_box`. false — метабокс отключен.
+            'show_admin_column'     => false, // авто-создание колонки таксы в таблице ассоциированного типа записи. (с версии 3.5)
+            'show_in_rest'          => null, // добавить в REST API
+            'rest_base'             => null, // $taxonomy
+            // '_builtin'              => false,
+            //'update_count_callback' => '_update_post_term_count',
+        ]);
     }
 
 }
