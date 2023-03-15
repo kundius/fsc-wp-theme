614f88a9a4ec601f0580387ba39f08ef854f9904
kundius <kundius.ruslan@gmail.com>
2023-03-15 16:39:59 +0300

    deploy

--- inc/Services/PostTypes.php
+++ inc/Services/PostTypes.php
@@ -26,14 +26,6 @@ class PostTypes implements Service
     public function boot(Service_Container $container): void
     {
         \add_action('init', [$this, 'register_post_types']);
-
-        // \acf_add_options_page(array(
-        //     'page_title' => 'Параметры',
-        //     'menu_title' => 'Параметры',
-        //     'menu_slug' => 'acf-options',
-        //     'capability' => 'edit_posts',
-        //     'redirect' => false,
-        // ));
     }
 
     /**
@@ -49,79 +41,61 @@ class PostTypes implements Service
         \register_post_type('project', [
             'label'  => null,
             'labels' => [
-                'name'               => 'Проект', // основное название для типа записи
-                'singular_name'      => 'Проект', // название для одной записи этого типа
-                'add_new'            => 'Добавить проект', // для добавления новой записи
-                'add_new_item'       => 'Добавление проекта', // заголовка у вновь создаваемой записи в админ-панели.
-                'edit_item'          => 'Редактирование проекта', // для редактирования типа записи
-                'new_item'           => 'Новый проект', // текст новой записи
-                'view_item'          => 'Смотреть проект', // для просмотра записи этого типа.
-                'search_items'       => 'Искать проект', // для поиска по этим типам записи
-                'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
-                'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
-                'parent_item_colon'  => 'Проект', // для родителей (у древовидных типов)
-                'menu_name'          => 'Проекты', // название меню
+                'name' => 'Проекты',
+                'all_items' => 'Все проекты',
+                'singular_name' => 'Проект',
+                'add_new' => 'Добавить новый',
+                'add_new_item' => 'Добавление проекта',
+                'edit_item' => 'Редактирование проекта',
+                'new_item' => 'Новый проект',
+                'view_item' => 'Смотреть проект',
+                'search_items' => 'Искать проект',
+                'not_found' => 'Не найдено',
+                'not_found_in_trash' => 'Не найдено в корзине',
+                'parent_item_colon' => 'Проект',
+                'menu_name' => 'Проекты',
             ],
-            'description'            => '',
-            'public'                 => true,
-            // 'publicly_queryable'  => null, // зависит от public
-            // 'exclude_from_search' => null, // зависит от public
-            // 'show_ui'             => null, // зависит от public
-            // 'show_in_nav_menus'   => null, // зависит от public
-            'show_in_menu'           => true, // показывать ли в меню админки
-            // 'show_in_admin_bar'   => null, // зависит от show_in_menu
-            'show_in_rest'        => null, // добавить в REST API. C WP 4.7
-            'rest_base'           => null, // $post_type. C WP 4.7
-            'menu_position'       => 9,
-            'menu_icon'           => null,
-            //'capability_type'   => 'post',
-            //'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
-            //'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
-            'hierarchical'        => false,
-            'supports'            => ['title', 'editor', 'thumbnail', 'excerpt'], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
-            'taxonomies'          => ['project_category'],
-            'has_archive'         => false,
-            'rewrite'             => true,
-            'query_var'           => true,
+            'description' => '',
+            'public' => true,
+            'show_in_menu' => true,
+            'show_in_rest' => null,
+            'rest_base' => null,
+            'menu_position' => 9,
+            'menu_icon' => null,
+            'hierarchical' => false,
+            'supports' => ['title', 'editor', 'thumbnail', 'excerpt'],
+            'taxonomies' => ['project_category'],
+            'has_archive' => false,
+            'rewrite' => true,
+            'query_var' => true,
         ]);
-
         
         register_taxonomy('project_category', ['project'], [
-            'label'                 => '', // определяется параметром $labels->name
-            'labels'                => [
-                'name'              => 'Категории',
-                'singular_name'     => 'Категория',
-                'search_items'      => 'Искать категории',
-                'all_items'         => 'Все категории',
-                'view_item '        => 'Смотреть категорию',
-                'parent_item'       => 'Родительская категория',
+            'label' => '',
+            'labels' => [
+                'name' => 'Категории',
+                'singular_name' => 'Категория',
+                'search_items' => 'Искать категории',
+                'all_items' => 'Все категории',
+                'view_item' => 'Смотреть категорию',
+                'parent_item' => 'Родительская категория',
                 'parent_item_colon' => 'Родительская категория:',
-                'edit_item'         => 'Редактировать категорию',
-                'update_item'       => 'Обновить категорию',
-                'add_new_item'      => 'Новая категория',
-                'new_item_name'     => 'Новое название категории',
-                'menu_name'         => 'Категории',
-                'back_to_items'     => 'Вернуться к категориям',
+                'edit_item' => 'Редактировать категорию',
+                'update_item' => 'Обновить категорию',
+                'add_new_item' => 'Новая категория',
+                'new_item_name' => 'Новое название категории',
+                'menu_name' => 'Категории',
+                'back_to_items' => 'Вернуться к категориям',
             ],
-            'description'           => '', // описание таксономии
-            'public'                => true,
-            // 'publicly_queryable'    => null, // равен аргументу public
-            // 'show_in_nav_menus'     => true, // равен аргументу public
-            // 'show_ui'               => true, // равен аргументу public
-            // 'show_in_menu'          => true, // равен аргументу show_ui
-            // 'show_tagcloud'         => true, // равен аргументу show_ui
-            // 'show_in_quick_edit'    => null, // равен аргументу show_ui
-            'hierarchical'          => false,
-
-            'rewrite'               => true,
-            //'query_var'             => $taxonomy, // название параметра запроса
-            'capabilities'          => [],
-            'meta_box_cb'           => 'post_categories_meta_box', // html метабокса. callback: `post_categories_meta_box` или `post_tags_meta_box`. false — метабокс отключен.
-            'show_admin_column'     => false, // авто-создание колонки таксы в таблице ассоциированного типа записи. (с версии 3.5)
-            'show_in_rest'          => null, // добавить в REST API
-            'rest_base'             => null, // $taxonomy
-            // '_builtin'              => false,
-            //'update_count_callback' => '_update_post_term_count',
+            'description' => '',
+            'public' => true,
+            'hierarchical' => false,
+            'rewrite' => true,
+            'capabilities' => [],
+            'meta_box_cb' => 'post_categories_meta_box',
+            'show_admin_column' => false,
+            'show_in_rest' => null,
+            'rest_base' => null,
         ]);
     }
 
