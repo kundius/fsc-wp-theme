d981ec43bd276b4245d51bb2e4371dd812efe401
kundius <kundius.ruslan@gmail.com>
2023-03-15 17:02:48 +0300

    deploy

---
+++ template-projects.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Template Name: Проекты
+ */
+?>
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes()?> itemscope itemtype="http://schema.org/WebSite">
+  <head>
+    <?php get_template_part('partials/head');?>
+  </head>
+  <body <?php body_class() ?>>
+    <?php wp_body_open() ?>
+
+    <div class="ui-wrapper">
+      <?php get_template_part('partials/header') ?>
+
+      <div class="page-main">
+        <div class="ui-container">
+          <h1 class="page-main__title"><?php the_title() ?></h1>
+
+          projects
+        </div>
+      </div>
+
+      <?php get_template_part('partials/footer')?>
+    </div>
+  </body>
+</html>
