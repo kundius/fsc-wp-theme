a218a966f01a8194c3e32aa41cc08e72c0d1362b
kundius <kundius.ruslan@gmail.com>
2023-03-15 17:03:09 +0300

    deploy

--- taxonomy-project_category.php
+++ taxonomy-project_category.php
@@ -1,1 +1,23 @@
-taxonomy-project_category
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes()?> itemscope itemtype="http://schema.org/WebSite">
+  <head>
+    <?php get_template_part('partials/head');?>
+  </head>
+  <body <?php body_class() ?>>
+    <?php wp_body_open() ?>
+
+    <div class="ui-wrapper">
+      <?php get_template_part('partials/header') ?>
+
+      <div class="page-main">
+        <div class="ui-container">
+          <h1 class="page-main__title"><?php the_title() ?></h1>
+
+          taxonomy-project_category
+        </div>
+      </div>
+
+      <?php get_template_part('partials/footer')?>
+    </div>
+  </body>
+</html>
