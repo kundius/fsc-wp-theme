9fee8b895bf98305e49d469e48902f0703d8ebad
kundius <kundius.ruslan@gmail.com>
2023-03-16 02:28:52 +0300

    deploy

--- template-project.php
+++ template-project.php
@@ -11,6 +11,16 @@ $nav = new WP_Query([
   'meta_key' => '_wp_page_template',
   'meta_value' => 'template-projects.php'
 ]);
+
+$pagelist = get_pages('sort_column=menu_order&sort_order=asc');
+$pages = [];
+foreach ($pagelist as $page) {
+   $pages[] += $page->ID;
+}
+
+$current = array_search(get_the_ID(), $pages);
+$prevID = $pages[$current-1];
+$nextID = $pages[$current+1];
 ?>
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes()?> itemscope itemtype="http://schema.org/WebSite">
@@ -77,7 +87,13 @@ $nav = new WP_Query([
 
             <div class="project-layout__neighbors">
               <div class="project-neighbors">
-                <?php posts_nav_link('', 'предыдущий', 'следующий') ?>
+                <?php if (!empty($prevID)): ?>
+                <a href="<?php echo get_permalink($prevID) ?>" title="<?php echo get_the_title($prevID) ?>" class="project-neighbors__prev">предыдущий</a>
+                <?php endif ?>
+                <div></div>
+                <?php if (!empty($nextID)): ?>
+                <a href="<?php echo get_permalink($nextID) ?>" title="<?php echo get_the_title($nextID) ?>" class="project-neighbors__next">следующий</a>
+                <?php endif ?>
               </div>
             </div>
           </div>
