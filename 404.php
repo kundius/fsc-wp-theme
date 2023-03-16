7bc67c7f13d397bf35a2e7e4a64c78b0ed25067f
kundius <kundius.ruslan@gmail.com>
2023-03-16 18:52:46 +0300

    deploy

--- 404.php
+++ 404.php
@@ -3,27 +3,29 @@
   <head>
     <?php get_template_part('partials/head');?>
   </head>
-  <body <?php body_class('page-404');?>>
+  <body <?php body_class();?>>
     <?php wp_body_open();?>
 
     <div class="ui-wrapper">
       <?php get_template_part('partials/header') ?>
 
-      <div class="not-found">
-        <div class="not-found__code">404</div>
-        <div class="not-found__body">
-          <div class="not-found__title">
-            Страница не найдена
-          </div>
-          <div class="not-found__desc">
-            Возможно, она была перемещена или удалена.<br />
-            <br />
-            Вернитесь назад или сообщите нам об ошибке
-          </div>
-          <div class="not-found__back">
-            <a href="#" class="ui-button-submit ui-button-submit_with-arrow">
-              Вернуться назад
-            </a>
+      <div class="page-not-found">
+        <div class="not-found">
+          <div class="not-found__code">404</div>
+          <div class="not-found__body">
+            <div class="not-found__title">
+              Страница не найдена
+            </div>
+            <div class="not-found__desc">
+              Возможно, она была перемещена или удалена.<br />
+              <br />
+              Вернитесь назад или сообщите нам об ошибке
+            </div>
+            <div class="not-found__back">
+              <a href="#" class="ui-button-submit ui-button-submit_with-arrow">
+                Вернуться назад
+              </a>
+            </div>
           </div>
         </div>
       </div>
